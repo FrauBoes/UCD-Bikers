b42ac344d89052d9a26495f98eb2180328b677b0
JuliaBoes <juliaboes@posteo.de>
2018-03-28 18:20:41 +0100

    worked on getDB for occupancy_graph

--- bikers/bikers/getDB.py
+++ bikers/bikers/getDB.py
@@ -2,38 +2,13 @@ import json
 import pymysql
 from builtins import str
 
-# RDS cannot connect
 host ="bikerz.cji0jregr8by.us-west-2.rds.amazonaws.com"
 port = 3306
 dbname="BikeData"
 user = "bikerz"
 password="bikerz123"
 
-# host = "localhost"
-# user = "root"
-# password = "112358"
-# dbname = "test_Dublinbikers"
-
-
     
-def getDB(number):
-    #lat = location["lat"]
-    #lan = number
-    conn = pymysql.connect(host, user=user, passwd=password,db=dbname)
-    cursor = conn.cursor()
-    
-    #sql = """SELECT STATUS FROM BIKEDATA WHERE STATION_NUMBER = {} """.format(number)
-    
-    sql = """SELECT available_bikes, store_time, status FROM BikeData WHERE number = {} ORDER BY store_time DESC LIMIT 3""".format(number)
-
-    cursor.execute(sql)
-    
-    data=cursor.fetchall()
-    
-    cursor.close()
-    
-    return str(data)
-
 # returns a string with all occupancy numbers of a given station for the current weekday
 def get_station_occupancy(number):
     conn = pymysql.connect(host, user=user, port=port, passwd=password,db=dbname)
@@ -49,6 +24,6 @@ def get_station_occupancy(number):
     
     return str(data)
     
-    
-if __name__=="__main__":
-    print(get_station_occupancy(37))
+   
+# if __name__=="__main__":
+#     print(get_station_occupancy(37))
--- bikers/bikers/views.py
+++ bikers/bikers/views.py
@@ -2,7 +2,6 @@ from flask import render_template, request
 
 from bikers import app
 
-
 from . import getStationsAPI
 
 from . import getWeatherAPI
@@ -12,16 +11,16 @@ from . import getDB
 @app.route('/')
 def index():
     app.logger.warning('sample message')
-    locations, number,bike_stands,available_bikes =getStationsAPI.initStations()
-    weather = getWeatherAPI.weatherbroadcast();
+    locations, number,bike_stands,available_bikes = getStationsAPI.initStations()
+    weather = getWeatherAPI.weatherbroadcast()
+    occupancy_graph(37)  ## default station 37 can be changed to dynamic later on
     return render_template('index.html',locations=locations,number=number,bike_stands=bike_stands,available_bikes=available_bikes,weather=weather)
 
 @app.route('/getdetail')
-def query():
+def get_station_no():
     number = request.args.get('num')
     occupancy_graph(number)
-    #print(getDB.getDB(number))
-    return occupancy_graph(number)
+    # return occupancy_graph(number)
 
 def occupancy_graph(number):
     occupancy_data = getDB.get_station_occupancy(number)
