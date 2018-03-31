c6e5c86aceba47049147d4ce6eb225b62dc251cd
JuliaBoes <juliaboes@posteo.de>
2018-03-31 15:18:14 +0100

    add sort data to getOccupancy.py

--- bikers/tests/test_bikers.py
+++ bikers/tests/test_bikers.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from bikers.bikers import getOccupancy
 sys.path.append('.')
 from bikers import bikers  
 import unittest
@@ -20,12 +21,13 @@ class BikersTestCase(unittest.TestCase):
         pass
 
     def test_occupancy(self):
-
-        
+        pass
+    
     def test_bucket_occupancy(self):
         pass
         
-
+    def test_convert_data(self):
+        assert getOccupancy.convert_data(getOccupancy.get_station_occupancy(1, 1)) is sorted
 
 if __name__ == "__main__":
     import sys;sys.argv = ['', 'Test.testName']
